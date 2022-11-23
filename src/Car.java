ae4f682848b406dcec055ad45c57be52d9d170b9
iShantaram <1agapovav987@gmail.com>
2022-11-23 13:57:45 +0300

    homework 1p3 completed

---
+++ src/Car.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private final double engineVolume;
+    private String color;
+    private final int productionYear;
+    private final String productionCountry;
+
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{ " + brand + model + ", сборка: " + productionCountry + ", " + productionYear + " год, цвет: " + color +
+                ", объем двигателя: " + engineVolume + "L }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Double.compare(car.engineVolume, engineVolume) == 0 && productionYear == car.productionYear && brand.equals(car.brand) && model.equals(car.model) && color.equals(car.color) && productionCountry.equals(car.productionCountry);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, productionYear, productionCountry);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+}
